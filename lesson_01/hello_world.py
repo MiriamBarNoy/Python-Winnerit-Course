be4ee4bf80ccc23f3942ef64b51d3a794d050a49
MiriamBarNoy <mb161287@gmail.com>
2024-09-17 21:55:34 +0300

    Adding new files

---
+++ lesson_01/hello_world.py
@@ -0,0 +1,14 @@
+import keyword
+
+
+print("Hello world")
+age = 36
+print(age)
+_item ="Miri"
+
+print(keyword.kwlist)
+my_global_item = 4555
+print(type(36))
+print(type(36.33))
+print(type(_item))
+
