4efde05bbf56d5a509642ff8e68f399825a307d6
MiriamBarNoy <mb161287@gmail.com>
2024-10-28 01:40:32 +0200

    Homework tasks for lesson 5 added

--- lesson_05/home_tasks/task_05_max_function.py
+++ lesson_05/home_tasks/task_05_max_function.py
@@ -1,10 +1,15 @@
+from itertools import count
+
+
 def find_max(*args):
-    count = int(input("Enter the number of values you want to check for max: "))
+
 # List to store the added numbers
     added_nums = []
-
+    count_i = int(input("Enter the number of values you want to check for max: "))
+    while count_i == 0:
+        return
  # Use a for loop with range to collect each number and add it to the list
-    for i in range(count):
+    for i in range(count_i):
         num_input = int(input(f"Enter number {i + 1}: "))
         added_nums.append(num_input)
         max_num = added_nums[0]
@@ -18,7 +23,10 @@ def find_max(*args):
 
 
 result = find_max()
-print(f"max nuber defined is {result}")
-
+if result == None:
+    print("no number provided")
+else:
+    print(f"the max number is {result}")
+#
 
 
